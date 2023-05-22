a9b42100f26c7a6111c3214d0b19ce29398d6b56
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2023-05-22 11:19:10 +0300

    Done password confirmation for /register

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
 
 class RegisterRequest extends FormRequest
 {
@@ -27,7 +28,7 @@ class RegisterRequest extends FormRequest
         return [
             'name' => 'required|string',
             'email' => 'required|email|unique:users,email',
-            'password' => 'required|string',
+            'password' => ['required','string','confirmed',Password::min(8)],
             'token_name' => ['required', Rule::in(['web'])]
         ];
     }
