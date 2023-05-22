52dea0df9c04d5d045e4ff37daf077af8b6bf4b1
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2023-05-22 11:07:08 +0300

    fixed response codes in CategoriesApiService

--- app/Services/Api/CategoriesApiService.php
+++ app/Services/Api/CategoriesApiService.php
@@ -34,7 +34,7 @@ class CategoriesApiService
 
     public function getById(int $id)
     {
-        $category =Category::find($id)->toArray();
+        $category = Category::find($id)->toArray();
         return response()->json($category);
     }
 
@@ -45,27 +45,27 @@ class CategoriesApiService
                 'name' => $request->name
             ]
         );
-        return response()->noContent();
+        return response()->noContent(201);
     }
 
     public function update(int $id, CategoryRequest $request)
     {
         Category::whereId($id)
             ->update($request->toArray());
-        return response()->noContent();
+        return response()->noContent(201);
     }
 
     public function destroyById(int $id)
     {
         Category::whereId($id)
             ->delete();
-        return response()->noContent();
+        return response()->noContent(201);
     }
 
     public function destroy()
     {
         DB::table('categories')->delete();
-        return response()->noContent();
+        return response()->noContent(201);
     }
 }
 
